c9d64e1fda13e9059e600bd7054168c50ef2fec2
Oliver Gaudreault <ogaudreault@blackberry.com>
2014-09-24 09:23:08 -0400

    Fix Method For getting distinct test stages for batch

--- REMI.Bll/TestStageManager.vb
+++ REMI.Bll/TestStageManager.vb
@@ -46,7 +46,7 @@ Namespace REMI.Bll
         <DataObjectMethod(DataObjectMethodType.[Select], False)> _
         Public Shared Function GetTestStagesNameByBatch(ByVal batchID As Int32) As Dictionary(Of String, String)
             Try
-                Return (From ts In New REMI.Dal.Entities().Instance().vw_GetTaskInfo Where ts.BatchID = batchID And ts.IsArchived = False And ts.TestIsArchived = False And ts.processorder > -1 Select ts).OrderBy(Function(o) o.processorder).ToDictionary(Function(k) k.TestStageID.ToString(), Function(v) v.tsname)
+                Return (From ts In New REMI.Dal.Entities().Instance().vw_GetTaskInfo Where ts.BatchID = batchID And ts.IsArchived = False And ts.TestIsArchived = False And ts.processorder > -1 Select ts.processorder, ts.TestStageID, ts.tsname).Distinct().OrderBy(Function(o) o.processorder).ToDictionary(Function(k) k.TestStageID.ToString(), Function(v) v.tsname)
             Catch ex As Exception
                 LogIssue(System.Reflection.MethodBase.GetCurrentMethod().Name, "e3", NotificationType.Errors, ex, String.Format("BatchID: {0}", batchID))
                 Return New Dictionary(Of String, String)
