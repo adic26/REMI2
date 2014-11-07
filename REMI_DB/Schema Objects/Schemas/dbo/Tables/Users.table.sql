62b4f0d5c0323984169d8fd528c0e67f3e14ed35
Oliver Gaudreault <ogaudreault@blackberry.com>
2014-11-07 10:26:57 -0500

    Update DB project with new table definition

--- REMI_DB/Schema Objects/Schemas/dbo/Tables/Users.table.sql
+++ REMI_DB/Schema Objects/Schemas/dbo/Tables/Users.table.sql
@@ -4,11 +4,9 @@
     [BadgeNumber]   INT            NULL,
     [ConcurrencyID] TIMESTAMP      NOT NULL,
     [LastUser]      NVARCHAR (255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-    [TestCentreID]  INT NULL,
 	[IsActive] [int] NOT NULL,
 	DefaultPage nvarchar(255) NULL,
-	ByPassProduct INT NULL,
-	DepartmentID INT NULL
+	ByPassProduct INT NULL
 );
 GO
 ALTER TABLE [dbo].[Users] ADD  DEFAULT ((1)) FOR [IsActive]
--- REMI_DB/Schema Objects/Schemas/dbo/Tables/UsersAudit.table.sql
+++ REMI_DB/Schema Objects/Schemas/dbo/Tables/UsersAudit.table.sql
@@ -6,10 +6,8 @@
     [UserName]    NVARCHAR (255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     [InsertTime]  DATETIME       NOT NULL,
     [Action]      CHAR (1)       COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-    [TestCentreID] INT NULL,
 	[IsActive] [int] NULL,
 	DefaultPage nvarchar(255) NULL,
-	ByPassProduct INT NULL,
-	DepartmentID INT NULL
+	ByPassProduct INT NULL
 );
 
