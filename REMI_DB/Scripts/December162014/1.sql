958a253b06932aebb7dd30c532144c0b277c567c
Oliver Gaudreault <ogaudreault@blackberry.com>
2014-12-16 08:50:52 -0500

    Update release script

--- REMI_DB/Scripts/December162014/1.sql
+++ REMI_DB/Scripts/December162014/1.sql
@@ -774,6 +774,201 @@ GRANT EXECUTE ON  [Req].[RequestSearch] TO [remi]
 GO
 GRANT CONTROL ON TYPE:: [dbo].[SearchFields] TO [remi]
 GO
+ALTER PROCEDURE [dbo].[remispBatchesInsertUpdateSingleItem]
+	@ID int OUTPUT,
+	@QRANumber nvarchar(11),
+	@Priority NVARCHAR(150) = 'NotSet', 
+	@BatchStatus int, 
+	@JobName nvarchar(400),
+	@TestStageName nvarchar(255)=null,
+	@ProductGroupName nvarchar(800),
+	@ProductType nvarchar(800),
+	@AccessoryGroupName nvarchar(800) = null,
+	@Comment nvarchar(1000) = null,
+	@TestCenterLocation nvarchar(400),
+	@RequestPurpose nvarchar(200),
+	@LastUser nvarchar(255),
+	@ConcurrencyID rowversion OUTPUT,
+	@testStageCompletionStatus int = null,
+	@requestor nvarchar(500) = null,
+	@unitsToBeReturnedToRequestor bit = null,
+	@expectedSampleSize int = null,
+	@reportApprovedDate datetime = null,
+	@reportRequiredBy datetime = null,
+	--@partName nvarchar(500) = null,
+	--@assemblyNumber nvarchar(500) = null,
+	--@assemblyRevision nvarchar(500) = null,
+	@reqStatus nvarchar(500) = null,
+	@cprNumber nvarchar(500) = null,
+	@pmNotes nvarchar(500) = null,
+	@MechanicalTools NVARCHAR(10) = null,
+	@RequestPurposeID int = 0,
+	@PriorityID INT = 0,
+	@DepartmentID INT = 0,
+	@Department NVARCHAR(150) = NULL,
+	@ExecutiveSummary NVARCHAR(4000) = NULL
+	AS
+	DECLARE @ProductID INT
+	DECLARE @ProductTypeID INT
+	DECLARE @AccessoryGroupID INT
+	DECLARE @TestCenterLocationID INT
+	DECLARE @ReturnValue int
+	DECLARE @maxid int
+	DECLARE @LookupTypeID INT
+	
+	IF NOT EXISTS (SELECT 1 FROM Products WHERE LTRIM(RTRIM(ProductGroupName))= LTRIM(RTRIM(@ProductGroupName)))
+	BEGIn
+		INSERT INTO Products (ProductGroupName) Values (LTRIM(RTRIM(@ProductGroupName)))
+	END
+	
+	IF NOT EXISTS (SELECT 1 FROM Lookups l INNER JOIN LookupType lt ON l.LookupTypeID=lt.LookupTypeID WHERE lt.Name='ProductType' AND LTRIM(RTRIM([Values])) = LTRIM(RTRIM(@ProductType)))
+	BEGIN
+		SELECT @LookupTypeID = LookupTypeID FROM LookupType WHERE Name='ProductType'
+		SELECT @maxid = MAX(LookupID)+1 FROM Lookups
+		INSERT INTO Lookups (LookupID, LookupTypeID, [Values]) Values (@maxid, @LookupTypeID, LTRIM(RTRIM(@ProductType)))
+	END
+	
+	IF LTRIM(RTRIM(@AccessoryGroupName)) <> '' AND NOT EXISTS (SELECT 1 FROM Lookups l INNER JOIN LookupType lt ON l.LookupTypeID=lt.LookupTypeID WHERE lt.Name='AccessoryType' AND LTRIM(RTRIM([Values])) = LTRIM(RTRIM(@AccessoryGroupName)))
+	BEGIN
+		SELECT @LookupTypeID = LookupTypeID FROM LookupType WHERE Name='AccessoryType'
+		SELECT @maxid = MAX(LookupID)+1 FROM Lookups
+		INSERT INTO Lookups (LookupID, LookupTypeID, [Values]) Values (@maxid, @LookupTypeID, LTRIM(RTRIM(@AccessoryGroupName)))
+	END
+	
+	IF LTRIM(RTRIM(@TestCenterLocation)) <> '' AND NOT EXISTS (SELECT 1 FROM Lookups l INNER JOIN LookupType lt ON l.LookupTypeID=lt.LookupTypeID WHERE lt.Name='TestCenter' AND LTRIM(RTRIM([Values])) = LTRIM(RTRIM(@TestCenterLocation)))
+	BEGIN
+		SELECT @LookupTypeID = LookupTypeID FROM LookupType WHERE Name='TestCenter'
+		SELECT @maxid = MAX(LookupID)+1 FROM Lookups
+		INSERT INTO Lookups (LookupID, LookupTypeID, [Values]) Values (@maxid, @LookupTypeID, LTRIM(RTRIM(@TestCenterLocation)))
+	END
+
+	IF LTRIM(RTRIM(@Department)) <> '' AND NOT EXISTS (SELECT 1 FROM Lookups l INNER JOIN LookupType lt ON l.LookupTypeID=lt.LookupTypeID WHERE lt.Name='Department' AND LTRIM(RTRIM([Values])) = LTRIM(RTRIM(@Department)))
+	BEGIN
+		SELECT @LookupTypeID = LookupTypeID FROM LookupType WHERE Name='Department'
+		SELECT @maxid = MAX(LookupID)+1 FROM Lookups
+		INSERT INTO Lookups (LookupID, LookupTypeID, [Values]) Values (@maxid, @LookupTypeID, LTRIM(RTRIM(@Department)))
+	END
+
+	IF LTRIM(RTRIM(@RequestPurpose)) <> '' AND NOT EXISTS (SELECT 1 FROM Lookups l INNER JOIN LookupType lt ON l.LookupTypeID=lt.LookupTypeID WHERE lt.Name='RequestPurpose' AND (LTRIM(RTRIM([Values])) = LTRIM(RTRIM(@RequestPurpose)) OR LTRIM(RTRIM([Description]))=LTRIM(RTRIM(@RequestPurpose))))
+	BEGIN
+		SELECT @LookupTypeID = LookupTypeID FROM LookupType WHERE Name='RequestPurpose'
+		SELECT @maxid = MAX(LookupID)+1 FROM Lookups
+		INSERT INTO Lookups (LookupID, LookupTypeID, [Values]) Values (@maxid, @LookupTypeID, LTRIM(RTRIM(@RequestPurpose)))
+	END
+
+	IF LTRIM(RTRIM(@Priority)) <> '' AND NOT EXISTS (SELECT 1 FROM Lookups l INNER JOIN LookupType lt ON l.LookupTypeID=lt.LookupTypeID WHERE lt.Name='Priority' AND LTRIM(RTRIM([Values])) = LTRIM(RTRIM(@Priority)))
+	BEGIN
+		SELECT @LookupTypeID = LookupTypeID FROM LookupType WHERE Name='Priority'
+		SELECT @maxid = MAX(LookupID)+1 FROM Lookups
+		INSERT INTO Lookups (LookupID, LookupTypeID, [Values]) Values (@maxid, @LookupTypeID, LTRIM(RTRIM(@Priority)))
+	END
+
+	SELECT @RequestPurposeID = LookupID FROM Lookups l INNER JOIN LookupType lt ON l.LookupTypeID=lt.LookupTypeID WHERE lt.Name='RequestPurpose' AND ([Values] = @RequestPurpose OR [Description] = @RequestPurpose)
+	SELECT @PriorityID = LookupID FROM Lookups l INNER JOIN LookupType lt ON l.LookupTypeID=lt.LookupTypeID WHERE lt.Name='Priority' AND [Values] = @Priority
+	SELECT @ProductID = ID FROM Products WITH(NOLOCK) WHERE LTRIM(RTRIM(ProductGroupName))= LTRIM(RTRIM(@ProductGroupName))
+	SELECT @ProductTypeID = LookupID FROM Lookups l WITH(NOLOCK) INNER JOIN LookupType lt WITH(NOLOCK) ON l.LookupTypeID=lt.LookupTypeID WHERE lt.Name='ProductType' AND LTRIM(RTRIM([Values]))= LTRIM(RTRIM(@ProductType))
+	SELECT @AccessoryGroupID = LookupID FROM Lookups l WITH(NOLOCK) INNER JOIN LookupType lt WITH(NOLOCK) ON l.LookupTypeID=lt.LookupTypeID WHERE lt.Name='AccessoryType' AND LTRIM(RTRIM([Values]))= LTRIM(RTRIM(@AccessoryGroupName))
+	SELECT @TestCenterLocationID = LookupID FROM Lookups l WITH(NOLOCK) INNER JOIN LookupType lt WITH(NOLOCK) ON l.LookupTypeID=lt.LookupTypeID WHERE lt.Name='TestCenter' AND LTRIM(RTRIM([Values]))= LTRIM(RTRIM(@TestCenterLocation))
+	SELECT @DepartmentID = LookupID FROM Lookups l WITH(NOLOCK) INNER JOIN LookupType lt WITH(NOLOCK) ON l.LookupTypeID=lt.LookupTypeID WHERE lt.Name='Department' AND LTRIM(RTRIM([Values]))= LTRIM(RTRIM(@Department))
+		
+	IF (@ID IS NULL)
+	BEGIN
+		INSERT INTO Batches(
+		QRANumber, 
+		Priority, 
+		BatchStatus, 
+		JobName,
+		TestStageName, 
+		ProductTypeID,
+		AccessoryGroupID,
+		TestCenterLocationID,
+		RequestPurpose,
+		Comment,
+		LastUser,
+		TestStageCompletionStatus,
+		Requestor,
+		unitsToBeReturnedToRequestor,
+		expectedSampleSize,
+		reportApprovedDate,
+		reportRequiredBy,
+		trsStatus,
+		cprNumber,
+		pmNotes,
+		ProductID, MechanicalTools, DepartmentID, ExecutiveSummary ) 
+		VALUES 
+		(@QRANumber, 
+		@PriorityID, 
+		@BatchStatus, 
+		@JobName,
+		@TestStageName,
+		@ProductTypeID,
+		@AccessoryGroupID,
+		@TestCenterLocationID,
+		@RequestPurposeID,
+		@Comment,
+		@LastUser,
+		@testStageCompletionStatus,
+		@Requestor,
+		@unitsToBeReturnedToRequestor,
+		@expectedSampleSize,
+		@reportApprovedDate,
+		@reportRequiredBy,
+		@reqStatus,
+		@cprNumber,
+		@pmNotes,
+		@ProductID, @MechanicalTools, @DepartmentID,@ExecutiveSummary)
+
+		SELECT @ReturnValue = SCOPE_IDENTITY()
+	END
+	ELSE -- Exisiting Item
+	BEGIN
+		UPDATE Batches SET 
+		QRANumber = @QRANumber, 
+		Priority = @PriorityID, 
+		Jobname = @Jobname, 
+		TestStagename = @TestStagename, 
+		BatchStatus = @BatchStatus, 
+		ProductTypeID = @ProductTypeID,
+		AccessoryGroupID = @AccessoryGroupID,
+		TestCenterLocationID=@TestCenterLocationID,
+		RequestPurpose=@RequestPurposeID,
+		Comment = @Comment, 
+		LastUser = @LastUser,
+		Requestor = @Requestor,
+		TestStageCompletionStatus = @testStageCompletionStatus,
+		unitsToBeReturnedToRequestor=@unitsToBeReturnedToRequestor,
+		expectedSampleSize=@expectedSampleSize,
+		reportApprovedDate=@reportApprovedDate,
+		reportRequiredBy=@reportRequiredBy,
+		trsStatus=@reqStatus,
+		cprNumber=@cprNumber,
+		pmNotes=@pmNotes ,
+		ProductID=@ProductID,
+		MechanicalTools = @MechanicalTools, DepartmentID = @DepartmentID,ExecutiveSummary=@ExecutiveSummary
+		WHERE (ID = @ID) AND (ConcurrencyID = @ConcurrencyID)
+
+		SELECT @ReturnValue = @ID
+	END
+	
+	IF EXISTS (SELECT 1 FROM Req.Request WHERE RequestNumber=@QRANumber)
+		BEGIN
+			UPDATE Req.Request SET BatchID=@ID WHERE RequestNumber=@QRANumber
+		END
+
+	SET @ConcurrencyID = (SELECT ConcurrencyID FROM Batches WITH(NOLOCK) WHERE ID = @ReturnValue)
+	SET @ID = @ReturnValue
+	
+	IF (@@ERROR != 0)
+	BEGIN
+		RETURN -1
+	END
+	ELSE
+	BEGIN
+		RETURN 0
+	END
+GO
+GRANT EXECUTE ON remispBatchesInsertUpdateSingleItem TO Remi
+GO
 IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
 GO
 IF @@TRANCOUNT>0 BEGIN
