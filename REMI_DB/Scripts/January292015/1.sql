d091495065e1f63e14bb32b18ff2b6f58f6b4010
Adi Chugh <achugh@blackberry.com>
2015-01-28 11:23:42 -0500

    new changes to DB scripts

--- REMI_DB/Scripts/January292015/1.sql
+++ REMI_DB/Scripts/January292015/1.sql
@@ -1,5 +1,7 @@
 BEGIN TRAN
 
+ALTER TABLE Products ADD LookupID INT
+go
 DECLARE @LookupTypeID INT
 DECLARE @MaxLookupID INT
 
@@ -16,11 +18,6 @@ INSERT INTO Lookups (LookupID, LookupTypeID, IsActive, [Values])
 SELECT @MaxLookupID + ROW_NUMBER() OVER (ORDER BY ID) AS RowID, @LookupTypeID AS LookupTypeID, p.IsActive, ProductGroupName
 FROM Products p
 
-ALTER TABLE Products ADD LookupID INT
-
-EXEC sp_RENAME 'Products.ProductGroupName' , '_ProductGroupName', 'COLUMN'
-EXEC sp_RENAME 'Products.IsActive' , '_IsActive', 'COLUMN'
-
 IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[DF__Products__IsActi__37D02F05]') AND type = 'D')
 BEGIN
 ALTER TABLE [dbo].[Products] DROP CONSTRAINT [DF__Products__IsActi__37D02F05]
@@ -29,13 +26,12 @@ END
 UPDATE p
 SET p.LookupID=l.LookupID
 FROM Lookups l
-inner join Products p on p._ProductGroupName=l.[Values]
+inner join Products p on p.ProductGroupName=l.[Values]
 WHERE l.LookupTypeID=@LookupTypeID
 
 update Req.ReqFieldSetup set OptionsTypeID=@LookupTypeID where Name like '%Product Group%'
 
 ALTER TABLE Products ALTER COLUMN LookupID INT NOT NULL
-ALTER TABLE Products ALTER COLUMN _IsActive BIT NULL
 
 IF  EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[Products]') AND name = N'UIX_Products_ProductGroupName')
 DROP INDEX [UIX_Products_ProductGroupName] ON [dbo].[Products] WITH ( ONLINE = OFF )
@@ -52,7 +48,15 @@ CREATE NONCLUSTERED INDEX [IX_Products_PG_IsActive] ON [dbo].[Products]
 INCLUDE ( [ID]) WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 GO
 
-ALTER TABLE Products ALTER COLUMN _ProductGroupName NVARCHAR(150) NULL
 ALTER TABLE dbo.UserDetails ADD IsAdmin BIT DEFAULT(0) NULL
 
-ROLLBACK TRAN
+go
+EXEC sp_RENAME 'Products.ProductGroupName' , '_ProductGroupName', 'COLUMN'
+EXEC sp_RENAME 'Products.IsActive' , '_IsActive', 'COLUMN'
+go
+
+ALTER TABLE Products ALTER COLUMN _ProductGroupName NVARCHAR(150) NULL
+go
+ALTER TABLE Products ALTER COLUMN _IsActive BIT NULL
+go
+COMMIT TRAN
