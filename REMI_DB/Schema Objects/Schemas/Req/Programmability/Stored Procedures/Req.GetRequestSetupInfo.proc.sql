07e87b129b81c2d706244dd17febc86bc510b7a8
Oliver Gaudreault <ogaudreault@blackberry.com>
2015-02-04 12:54:21 -0500

    Fix SP error

--- REMI_DB/Schema Objects/Schemas/Req/Programmability/Stored Procedures/Req.GetRequestSetupInfo.proc.sql
+++ REMI_DB/Schema Objects/Schemas/Req/Programmability/Stored Procedures/Req.GetRequestSetupInfo.proc.sql
@@ -18,7 +18,7 @@ BEGIN
 				INNER JOIN Tests t WITH(NOLOCK) ON ( ( ts.teststagetype = 2 AND ts.testid = t.id ) OR ts.teststagetype != 2 AND ts.teststagetype = t.testtype )
 				LEFT OUTER JOIN Req.RequestSetup rs ON rs.JobID=@JobID AND rs.ProductID=@ProductID AND rs.TestID=t.ID AND rs.TestStageID=ts.ID
 			WHERE j.ID=@JobID AND ts.TestStageType=@TestStageType AND ts.ProcessOrder >= 0 AND ISNULL(ts.IsArchived, 0) = 0 AND ISNULL(t.IsArchived, 0) = 0
-				AND (@TestStageType = 1 AND t.ID IN (SELECT TestID FROM TestsAccess ta INNER JOIN #Departments d ON d.LookupID=ta.LookupID))
+				AND (@TestStageType <> 1 OR (@TestStageType = 1 AND t.ID IN (SELECT TestID FROM TestsAccess ta INNER JOIN #Departments d ON d.LookupID=ta.LookupID)))
 			ORDER BY ts.ProcessOrder, t.TestName
 		END
 		ELSE IF @BlankSelected = 0 AND EXISTS(SELECT 1 FROM Req.RequestSetup rs INNER JOIN Tests t ON t.ID=rs.TestID WHERE JobID=@JobID AND ProductID IS NULL AND t.TestType=@TestStageType)
@@ -29,7 +29,7 @@ BEGIN
 				INNER JOIN Tests t WITH(NOLOCK) ON ( ( ts.teststagetype = 2 AND ts.testid = t.id ) OR ts.teststagetype != 2 AND ts.teststagetype = t.testtype )
 				LEFT OUTER JOIN Req.RequestSetup rs ON rs.JobID=@JobID AND rs.ProductID IS NULL AND rs.TestID=t.ID AND rs.TestStageID=ts.ID
 			WHERE j.ID=@JobID AND ts.TestStageType=@TestStageType AND ts.ProcessOrder >= 0 AND ISNULL(ts.IsArchived, 0) = 0 AND ISNULL(t.IsArchived, 0) = 0
-				AND (@TestStageType = 1 AND t.ID IN (SELECT TestID FROM TestsAccess ta INNER JOIN #Departments d ON d.LookupID=ta.LookupID))
+				AND (@TestStageType <> 1 OR (@TestStageType = 1 AND t.ID IN (SELECT TestID FROM TestsAccess ta INNER JOIN #Departments d ON d.LookupID=ta.LookupID)))
 			ORDER BY ts.ProcessOrder, t.TestName
 		END
 		ELSE
@@ -39,7 +39,7 @@ BEGIN
 				INNER JOIN TestStages ts ON j.ID=ts.JobID
 				INNER JOIN Tests t WITH(NOLOCK) ON ( ( ts.teststagetype = 2 AND ts.testid = t.id ) OR ts.teststagetype != 2 AND ts.teststagetype = t.testtype )
 			WHERE j.ID=@JobID AND ts.TestStageType=@TestStageType AND ts.ProcessOrder >= 0 AND ISNULL(ts.IsArchived, 0) = 0 AND ISNULL(t.IsArchived, 0) = 0
-				AND (@TestStageType = 1 AND t.ID IN (SELECT TestID FROM TestsAccess ta INNER JOIN #Departments d ON d.LookupID=ta.LookupID))
+				AND (@TestStageType <> 1 OR (@TestStageType = 1 AND t.ID IN (SELECT TestID FROM TestsAccess ta INNER JOIN #Departments d ON d.LookupID=ta.LookupID)))
 			ORDER BY ts.ProcessOrder, t.TestName
 		END
 	END
@@ -59,7 +59,7 @@ BEGIN
 												(rs.ProductID IS NULL)
 											)
 		WHERE j.ID=@JobID AND ts.TestStageType=@TestStageType AND ts.ProcessOrder >= 0 AND ISNULL(ts.IsArchived, 0) = 0 AND ISNULL(t.IsArchived, 0) = 0
-			AND (@TestStageType = 1 AND t.ID IN (SELECT TestID FROM TestsAccess ta INNER JOIN #Departments d ON d.LookupID=ta.LookupID))
+			AND (@TestStageType <> 1 OR (@TestStageType = 1 AND t.ID IN (SELECT TestID FROM TestsAccess ta INNER JOIN #Departments d ON d.LookupID=ta.LookupID)))
 		ORDER BY ts.ProcessOrder, t.TestName		
 	END
 	
