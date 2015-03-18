1187665a80f696672507a594bb358238cc8421db
Oliver Gaudreault <ogaudreault@blackberry.com>
2015-03-18 15:57:02 -0400

    Update SP

--- REMI_DB/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/remispGetServicesAccessByID.sql
+++ REMI_DB/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/remispGetServicesAccessByID.sql
@@ -4,13 +4,13 @@ BEGIN
 	DECLARE @TrueBit BIT
 	SET @TrueBit = CONVERT(BIT, 1)
 	
-	SELECT 0 AS ServiceID, '' AS ServiceName, 0 AS ServiceAccessID, '' AS [Values]
+	SELECT 0 AS ServiceID, '' AS ServiceName, 0 AS ServiceAccessID, '' AS [Values], 0 AS DepartmentID
 	UNION
-	SELECT s.ServiceID, s.ServiceName, sa.ServiceAccessID, ld.[Values]
+	SELECT s.ServiceID, s.ServiceName, sa.ServiceAccessID, ld.[Values], ld.LookupID AS DepartmentID
 	FROM dbo.Services s
 		INNER JOIN dbo.ServicesAccess sa WITH (NOLOCK) ON sa.ServiceID=s.ServiceID
 		INNER JOIN Lookups ld WITH(NOLOCK) ON ld.LookupID=sa.LookupID
-	WHERE (@LookupID IS NULL OR sa.LookupID=@LookupID) AND ISNULL(s.IsActive, 0) = @TrueBit
+	WHERE (ISNULL(@LookupID, 0) = 0 OR sa.LookupID=@LookupID) AND ISNULL(s.IsActive, 0) = @TrueBit
 END
 GO
 GRANT EXECUTE ON dbo.remispGetServicesAccessByID TO REMI
