1b0016dfce9feb5b25b6bf30f7fe45e5de1faf63
Oliver Gaudreault <ogaudreault@blackberry.com>
2015-04-30 12:09:15 -0400

    Fixed the ordering of the lookup list

--- REMI_DB/Scripts/2015/May192015/2.sql
+++ REMI_DB/Scripts/2015/May192015/2.sql
@@ -3619,4 +3619,22 @@ END
 GO
 GRANT EXECUTE ON [Req].[RequestGet] TO REMI
 GO
+ALTER PROCEDURE remispGetLookupTypes @ShowSystemTypes BIT
+AS
+BEGIN
+	SELECT 0 AS LookupTypeID, 'SELECT...' AS Name, 0 As IsSystem
+	UNION ALL
+	SELECT lt.LookupTypeID, lt.Name, IsSystem
+	FROM LookupType lt
+	WHERE 
+		(
+			@ShowSystemTypes = 1
+			OR
+			lt.IsSystem=@ShowSystemTypes
+		)
+	ORDER BY Name
+END
+GO
+GRANT EXECUTE ON remispGetLookupTypes TO REMI
+GO
 rollback tran
