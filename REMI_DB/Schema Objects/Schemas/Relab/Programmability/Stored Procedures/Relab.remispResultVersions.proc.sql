b9c17d87138087cdeb9aefc40eb434d0e952200e
Oliver Gaudreault <ogaudreault@blackberry.com>
2015-05-12 08:32:05 -0400

    RELISSUE-1614: Add errorocurred field on versions.aspx page.

--- REMI_DB/Schema Objects/Schemas/Relab/Programmability/Stored Procedures/Relab.remispResultVersions.proc.sql
+++ REMI_DB/Schema Objects/Schemas/Relab/Programmability/Stored Procedures/Relab.remispResultVersions.proc.sql
@@ -4,7 +4,7 @@ BEGIN
 	SELECT tu.BatchUnitNumber, ts.TestStageName As TestStage, rxml.ResultXML, rxml.StationName, rxml.StartDate, rxml.EndDate, ISNULL(rxml.lossFile,'') AS lossFile, 
 		CASE WHEN rxml.isProcessed = 1 THEN 'Yes' ELSE 'No' END As Processed, rxml.VerNum, 
 		ISNULL(rxml.ProductXML, '') AS ProductXML, ISNULL(rxml.StationXML, '') AS StationXML, ISNULL(rxml.SequenceXML, '') AS SequenceXML,
-		ISNULL(rxml.TestXML, '') AS TestXML
+		ISNULL(rxml.TestXML, '') AS TestXML, CASE WHEN rxml.ErrorOccured = 1 THEN 'Yes' ELSE 'No' END As ErrorOccured
 	FROM Relab.Results r WITH(NOLOCK)
 		INNER JOIN TestUnits tu WITH(NOLOCK) ON r.TestUnitID=tu.ID
 		INNER JOIN Relab.ResultsXML rxml WITH(NOLOCK) ON r.ID=rxml.ResultID
--- REMI_DB/Scripts/2015/May192015/1.sql
+++ REMI_DB/Scripts/2015/May192015/1.sql
@@ -418,4 +418,22 @@ group by lp.[Values];
 GO
 GRANT EXECUTE ON remispInventoryReport TO Remi
 GO
+ALTER PROCEDURE Relab.remispResultVersions  @TestID INT, @BatchID INT, @UnitNumber INT = 0, @TestStageID INT = 0
+AS
+BEGIN
+	SELECT tu.BatchUnitNumber, ts.TestStageName As TestStage, rxml.ResultXML, rxml.StationName, rxml.StartDate, rxml.EndDate, ISNULL(rxml.lossFile,'') AS lossFile, 
+		CASE WHEN rxml.isProcessed = 1 THEN 'Yes' ELSE 'No' END As Processed, rxml.VerNum, 
+		ISNULL(rxml.ProductXML, '') AS ProductXML, ISNULL(rxml.StationXML, '') AS StationXML, ISNULL(rxml.SequenceXML, '') AS SequenceXML,
+		ISNULL(rxml.TestXML, '') AS TestXML, CASE WHEN rxml.ErrorOccured = 1 THEN 'Yes' ELSE 'No' END As ErrorOccured
+	FROM Relab.Results r WITH(NOLOCK)
+		INNER JOIN TestUnits tu WITH(NOLOCK) ON r.TestUnitID=tu.ID
+		INNER JOIN Relab.ResultsXML rxml WITH(NOLOCK) ON r.ID=rxml.ResultID
+		INNER JOIN TestStages ts WITH(NOLOCK) ON ts.ID=r.TestStageID
+	WHERE r.TestID=@TestID AND tu.BatchID=@BatchID
+		AND (@UnitNumber = 0 OR tu.BatchUnitNumber=@UnitNumber)
+		AND (@TestStageID = 0 OR ts.ID=@TestStageID)
+END
+GO
+GRANT EXECUTE ON [Relab].[remispResultVersions] TO Remi
+GO
 rollback tran
